70567bd0978cff500408381cb12a85c5ce2a2cc6
hardik-joshi <joshi.hardik654@gmail.com>
2017-09-24 13:17:11 +0530

    step-4: Directory and File Organization

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
 'use strict';
 
-angular.module('phonecatApp', []);
+angular.module('phonecatApp', [
+    'phoneList'
+]);
--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,16 +1,10 @@
 'use strict';
 
 angular.
-    module('phonecatApp').
-    component('phoneList',{
-        template:
-            '<ul>' +
-                '<li ng-repeat="phone in $ctrl.phones">' +
-                    '<span>{{phone.name}}</span>' +
-                    '<p>{{phone.snippet}}</p>' +
-                '</li>' +
-            '</ul>',
-        controller: function PhoneListController(){
+    module('phoneList').
+    component('phoneList', {
+        templateUrl: 'phone-list/phone-list.template.html',
+        controller: function PhoneListController() {
             this.phones = [
                 {
                     name: 'Nexus X',
@@ -26,4 +20,4 @@ angular.
                 }
             ];
         }
-    })
+    });
