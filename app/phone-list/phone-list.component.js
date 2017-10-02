9aa86d29cf3cb35fb5f439971cc65aaf07187c3b
hardik-joshi <joshi.hardik654@gmail.com>
2017-10-02 19:38:39 +0530

    XHR & Dependency Injection

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -4,25 +4,15 @@ angular.
     module('phoneList').
     component('phoneList', {
         templateUrl: 'phone-list/phone-list.template.html',
-        controller: function PhoneListController() {
-            this.phones = [
-                {
-                    name: 'Nexus X',
-                    snippet: 'Fast just got faster with Nexus S.',
-                    age: 0
-                },
-                {
-                    name: 'Motorola XOOM™ with Wi-Fi',
-                    snippet: 'The Next, Next Generation tablet.',
-                    age: 1
-                },
-                {
-                    name: 'MOTOROLA XOOM™',
-                    snippet: 'The Next, Next Generation tablet.',
-                    age: 2
-                }
-            ];
-            
-            this.orderProp = 'age';
-        }
+        controller: ['$http', 
+            function PhoneListController($http) {
+                var self = this;
+                
+                self.orderProp = 'age';
+
+                $http.get('phones/phones.json').then(function(response){
+                    self.phones = response.data;
+                });
+            }
+        ]
     });
--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -7,6 +7,20 @@ describe('Phone Catalog Application', function(){
         beforeEach(function(){
            browser.get('index.html');
         });
+
+        it('should filter the phone list as a user types into the search box', function(){
+            var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
+            var query = element(by.model('$ctrl.query'));
+
+            expect(phoneList.count()).toBe(20);
+
+            query.sendKeys('nexus');
+            expect(phoneList.count()).toBe(1);
+
+            query.clear();
+            query.sendKeys('motorola');
+            expect(phoneList.count()).toBe(8);
+        });
         
         it('should be possible to control phone order via the drop-down menu',function(){
             var queryField = element(by.model('$ctrl.query'));
