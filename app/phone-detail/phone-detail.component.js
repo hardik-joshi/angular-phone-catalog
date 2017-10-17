a2fb83a578000f2f5a5e43089a7f30c215b4c840
hardik-joshi <joshi.hardik654@gmail.com>
2017-10-17 18:07:55 +0530

    step-13 REST and Custom Services

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -4,18 +4,17 @@ angular.
 	module('phoneDetail').
 	component('phoneDetail',{
 		templateUrl: 'phone-detail/phone-detail.template.html',
-		controller: ['$http','$routeParams',
-			function phoneDetailController($http,$routeParams){
+		controller: ['$routeParams','Phone',
+			function phoneDetailController($routeParams,Phone){
                 var self = this;
                 
-                self.setImage = function(imageUrl){
+                self.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone){
+                    self.setImage(phone.images[0]);
+                });
+                
+                self.setImage = function setImage(imageUrl){
                     self.mainImageUrl = imageUrl;    
                 }
-                
-				$http.get('phones/' + $routeParams.phoneId + '.json').then(function(response){
-                    self.phone = response.data; 
-                    self.setImage(self.phone.images[0]);
-                });
 			}
 		]
 	});
--- app/phone-detail/phone-detail.module.js
+++ app/phone-detail/phone-detail.module.js
@@ -1,5 +1,6 @@
 'use strict';
 
 angular.module('phoneDetail',[
-	'ngRoute'
+	'ngRoute',
+    'core.phone'
 ]);
